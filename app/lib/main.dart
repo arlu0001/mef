e78e1f563c4c97beac0e88cda4ba47137c9477ae
arlu0001 <arlu0001@stud.hs-kl.de>
2021-06-05 13:44:37 +0200

    Add platform check to prevent users from starting the trial on desktop

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,5 @@
+import "package:universal_html/html.dart";
+
 import 'package:app/post_trial/color_blindness_test_screen.dart';
 import 'package:app/post_trial/final_screen.dart';
 import 'package:app/post_trial/survey_screen_2.dart';
@@ -18,30 +20,44 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final SurveyState surveyState = SurveyState();
+  final userAgent = window.navigator.userAgent.toString().toLowerCase();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: welcomeScreen,
-      routes: {
-        welcomeScreen: (context) => WelcomeScreen(),
-        surveyScreen1: (context) => ChangeNotifierProvider<SurveyState>(
-              create: (_) => surveyState,
-              child: SurveyScreen1(),
+    if (userAgent.contains('iphone') || userAgent.contains('ipad') || userAgent.contains('android')) {
+      return MaterialApp(
+        initialRoute: welcomeScreen,
+        routes: {
+          welcomeScreen: (context) => WelcomeScreen(),
+          surveyScreen1: (context) => ChangeNotifierProvider<SurveyState>(
+                create: (_) => surveyState,
+                child: SurveyScreen1(),
+              ),
+          surveyScreen2: (context) => ChangeNotifierProvider<SurveyState>(
+                create: (_) => surveyState,
+                child: SurveyScreen2(),
+              ),
+          trialExplanationScreen: (context) => TrialExplanationScreen(),
+          trialExampleScreen: (context) => TrialExampleScreen(),
+          trialScreen: (context) => ChangeNotifierProvider<TrialState>(
+                create: (_) => TrialState(),
+                child: TrialScreen(),
+              ),
+          colorBlindnessTestScreen: (context) => ColorBlindnessTestScreen(),
+          finalScreen: (context) => FinalScreen(),
+        },
+      );
+    } else {
+      return MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              'Dieses Experiment untersucht das Nutzerverhalten auf Smartphones. \nBitte rufen Sie diesen Link auf einem Smartphone auf, welches Android oder IOS als Betriebssystem nutzt. \nVielen Dank für Ihr Verständnis!',
+              textAlign: TextAlign.center,
             ),
-        surveyScreen2: (context) => ChangeNotifierProvider<SurveyState>(
-              create: (_) => surveyState,
-              child: SurveyScreen2(),
-            ),
-        trialExplanationScreen: (context) => TrialExplanationScreen(),
-        trialExampleScreen: (context) => TrialExampleScreen(),
-        trialScreen: (context) => ChangeNotifierProvider<TrialState>(
-              create: (_) => TrialState(),
-              child: TrialScreen(),
-            ),
-        colorBlindnessTestScreen: (context) => ColorBlindnessTestScreen(),
-        finalScreen: (context) => FinalScreen(),
-      },
-    );
+          ),
+        ),
+      );
+    }
   }
 }
