00950b4e4401e26064cfe738d45500822ea4224a
raubertochter <75852047+raubertochter@users.noreply.github.com>
2021-07-15 14:48:09 +0200

    Plots und Subsets für Probanden mit Farbfehlsichtigkeit

--- MEF/Datasets_Subsets.R
+++ MEF/Datasets_Subsets.R
@@ -37,7 +37,7 @@ data_ishiharafalse <-subset(data, data$`Ishihara 3` != 3 | data$`Ishihara 42` !=
 
 
 #Subsets nach Uhrzeit 
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 library(tidyverse)
 
 data_ishiharaclean$'Uhrzeit' <- format(as.POSIXct(data_ishiharaclean$'Uhrzeit'), "%H:%M:%S")
@@ -49,6 +49,18 @@ data_ic_e <-subset(data_ishiharaclean, data_ishiharaclean$'Uhrzeit' >= '18:00:00
 
 data_ic_n <-subset(data_ishiharaclean, data_ishiharaclean$'Uhrzeit' >= '00:00:00' & data_ishiharaclean$'Uhrzeit'<= '05:59:59')
 
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+library(tidyverse)
+
+data_ishiharafalse$'Uhrzeit' <- format(as.POSIXct(data_ishiharafalse$'Uhrzeit'), "%H:%M:%S")
+
+data_if_mo <-subset(data_ishiharafalse, data_ishiharafalse$'Uhrzeit' >= "06:00:00" & data_ishiharafalse$'Uhrzeit'<= "11:59:59")
+
+data_if_a <-subset(data_ishiharafalse, data_ishiharafalse$'Uhrzeit' >= "12:00:00" & data_ishiharafalse$'Uhrzeit'<= "17:59:59" )
+
+data_if_e <-subset(data_ishiharafalse, data_ishiharafalse$'Uhrzeit' >= '18:00:00' & data_ishiharaclean$'Uhrzeit'<= '23:59:59')
+
+data_if_n <-subset(data_ishiharafalse, data_ishiharafalse$'Uhrzeit' >= '00:00:00' & data_ishiharafalse$'Uhrzeit'<= '05:59:59')
 
 ## Unbereinigt
 data$'Uhrzeit' <- format(as.POSIXct(data$'Uhrzeit'), "%H:%M:%S")
@@ -63,17 +75,22 @@ data_n <-subset(data, data$'Uhrzeit' >= '00:00:00' & data$'Uhrzeit'<= '05:59:59'
 
 
 # Subset nach Geschlecht
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 data_ic_m <-subset(data_ishiharaclean, data_ishiharaclean$'Geschlecht' == 'm')
 data_ic_f <-subset(data_ishiharaclean, data_ishiharaclean$'Geschlecht' == 'f')
+
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+data_if_m <-subset(data_ishiharafalse, data_ishiharafalse$'Geschlecht' == 'm')
+data_if_f <-subset(data_ishiharafalse, data_ishiharafalse$'Geschlecht' == 'f')
 
 ## Unbereinigt
 data_m <-subset(data, data$'Geschlecht' == 'm')
 data_f <-subset(data, data$'Geschlecht' == 'f')
 
 
+
 # Subsets nach Alter
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 data_ic_11 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == 'unter 11')
 data_ic_1120 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == '11-20')
 data_ic_2130 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == '21-30')
@@ -81,6 +98,15 @@ data_ic_3140 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == '31-40')
 data_ic_4150 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == '41-50')
 data_ic_5160 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == '51-60')
 data_ic_60 <-subset(data_ishiharaclean, data_ishiharaclean$'Alter' == 'über 60')
+
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+data_if_11 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == 'unter 11')
+data_if_1120 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == '11-20')
+data_if_2130 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == '21-30')
+data_if_3140 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == '31-40')
+data_if_4150 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == '41-50')
+data_if_5160 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == '51-60')
+data_if_60 <-subset(data_ishiharafalse, data_ishiharafalse$'Alter' == 'über 60')
 
 ## Unbereinigt
 data_11 <-subset(data, data$'Alter' == 'unter 11')
@@ -92,31 +118,32 @@ data_5160 <-subset(data, data$'Alter' == '51-60')
 data_60 <-subset(data, data$'Alter' == 'über 60')
 
 
+
 # Subsets nach OS
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 data_ic_iOS <-subset(data_ishiharaclean, data_ishiharaclean$'OS' == 'iOS')
 data_ic_Android <-subset(data_ishiharaclean, data_ishiharaclean$'OS' == 'Android')
+
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+data_if_iOS <-subset(data_ishiharafalse, data_ishiharafalse$'OS' == 'iOS')
+data_if_Android <-subset(data_ishiharafalse, data_ishiharafalse$'OS' == 'Android')
 
 ## Unbereinigt
 data_iOS <-subset(data, data$'OS' == 'iOS')
 data_Android <-subset(data, data$'OS' == 'Android')
 
-
-# Subsets nach OS
-## Bereinigt
-data_ic_iOS <-subset(data_ishiharaclean, data_ishiharaclean$'OS' == 'iOS')
-data_ic_Android <-subset(data_ishiharaclean, data_ishiharaclean$'OS' == 'Android')
-
-## Unbereinigt
-data_iOS <-subset(data, data$'OS' == 'iOS')
-data_Android <-subset(data, data$'OS' == 'Android')
 
 
 # Subsets nach Smartphone Nutzung
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 data_ic_mt <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzung' == 'mehrmals täglich')
 data_ic_t <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzung' == 'täglich')
 data_ic_mw <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzung' == 'mehrmals die Woche')
+
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+data_if_mt <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzung' == 'mehrmals täglich')
+data_if_t <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzung' == 'täglich')
+data_if_mw <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzung' == 'mehrmals die Woche')
 
 ## Unbereinigt
 data_mt <-subset(data, data$'Smartphone Nutzung' == 'mehrmals täglich')
@@ -124,13 +151,21 @@ data_t <-subset(data, data$'Smartphone Nutzung' == 'täglich')
 data_mw <-subset(data, data$'Smartphone Nutzung' == 'mehrmals die Woche')
 
 
+
 # Subsets nach Smartphone Erfahrung
-## Bereinigt
+## Bereinigt nur Probanden ohne Farbfehlsichtigkeit
 data_ic_ss <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzungserfahrung' == 'sehr sicher')
 data_ic_s <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzungserfahrung' == 'sicher')
 data_ic_mm <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzungserfahrung' == 'mittelmäßig')
 data_ic_u <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzungserfahrung' == 'unsicher')
 data_ic_su <-subset(data_ishiharaclean, data_ishiharaclean$'Smartphone Nutzungserfahrung' == 'sehr unsicher')
+
+## Bereinigt nur Probanden mit Farbfehlsichtigkeit
+data_if_ss <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzungserfahrung' == 'sehr sicher')
+data_if_s <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzungserfahrung' == 'sicher')
+data_if_mm <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzungserfahrung' == 'mittelmäßig')
+data_if_u <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzungserfahrung' == 'unsicher')
+data_if_su <-subset(data_ishiharafalse, data_ishiharafalse$'Smartphone Nutzungserfahrung' == 'sehr unsicher')
 
 ## Unbereinigt
 data_ss <-subset(data, data$'Smartphone Nutzungserfahrung' == 'sehr sicher')
@@ -166,7 +201,7 @@ get_tptfvr <- function(dataset){
   return(mean_tpt_value)
 }
 
-## Bereinigt
+## Bereinigt ohne Farbfehlsichtigkeit
 #Millisekunden
 mean_tpt <- get_tptfv(data_ishiharaclean)
 #Sekunden
@@ -252,4 +287,92 @@ mean_tptr_mo <- get_tptfvr(data_ic_mo)
 mean_tptr_a <- get_tptfvr(data_ic_a)
 mean_tptr_e <- get_tptfvr(data_ic_e)
 mean_tptr_n <- get_tptfvr(data_ic_n)
+
+
+## Bereinigt mit Farbfehlsichtigkeit
+#Millisekunden
+mean_tpt_if <- get_tptfv(data_ishiharafalse)
+#Sekunden
+mean_tptr_if <- get_tptfvr(data_ishiharafalse)
+
+
+## Bereinigt nach Geschlecht
+#Millisekunden
+mean_tpt_if_f<- get_tptfv(data_if_f)
+mean_tpt_if_m<- get_tptfv(data_if_m)
+
+#Sekunden
+mean_tptr_if_f<- get_tptfvr(data_if_f)
+mean_tptr_if_m<- get_tptfvr(data_if_m)
+
+
+## Bereinigt nach Alter
+#Millisekunden
+mean_tpt_if_11<- get_tptfv(data_if_11)
+mean_tpt_if_1120<- get_tptfv(data_if_1120)
+mean_tpt_if_2130<- get_tptfv(data_if_2130)
+mean_tpt_if_3140<- get_tptfv(data_if_3140)
+mean_tpt_if_4150<- get_tptfv(data_if_4150)
+mean_tpt_if_5160<- get_tptfv(data_if_5160)
+mean_tpt_if_60<- get_tptfv(data_if_60)
+#Sekunden
+mean_tptr_if_11<- get_tptfvr(data_if_11)
+mean_tptr_if_1120<- get_tptfvr(data_if_1120)
+mean_tptr_if_2130<- get_tptfvr(data_if_2130)
+mean_tptr_if_3140<- get_tptfvr(data_if_3140)
+mean_tptr_if_4150<- get_tptfvr(data_if_4150)
+mean_tptr_if_5160<- get_tptfvr(data_if_5160)
+mean_tptr_if_60<- get_tptfvr(data_if_60)
+
+
+## Bereinigt nach Betriebssystem
+#Millisekunden
+mean_tpt_if_iOS<- get_tptfv(data_if_iOS)
+mean_tpt_if_Android<- get_tptfv(data_if_Android)
+
+#Sekunden
+mean_tptr_if_iOS<- get_tptfvr(data_if_iOS)
+mean_tptr_if_Android<- get_tptfvr(data_if_Android)
+
+
+## Bereinigt nach Smartphone Nutzung
+#Millisekunden
+mean_tpt_if_mt<- get_tptfv(data_if_mt)
+mean_tpt_if_t<- get_tptfv(data_if_t)
+#mean_tpt_if_wt<- get_tptfv(data_if_wt)
+
+#Sekunden
+mean_tptr_if_mt<- get_tptfvr(data_if_mt)
+mean_tptr_if_t<- get_tptfvr(data_if_t)
+#mean_tptr_if_wt<- get_tptfvr(data_if_wt)
+
+
+## Bereinigt nach Smartphone Erfahrung
+#Millisekunden
+mean_tpt_if_ss<- get_tptfv(data_if_ss)
+mean_tpt_if_s<- get_tptfv(data_if_s)
+mean_tpt_if_mm<- get_tptfv(data_if_mm)
+mean_tpt_if_u<- get_tptfv(data_if_u)
+mean_tpt_if_su<- get_tptfv(data_if_su)
+
+#Sekunden
+mean_tptr_if_ss<- get_tptfvr(data_if_ss)
+mean_tptr_if_s<- get_tptfvr(data_if_s)
+mean_tptr_if_mm<- get_tptfvr(data_if_mm)
+mean_tptr_if_u<- get_tptfvr(data_if_u)
+mean_tptr_if_su<- get_tptfvr(data_if_su)
+
+## Bereinigt nach Uhrzeit
+
+#Millisekunden
+mean_tpt_if_mo <- get_tptfv(data_if_mo)
+mean_tpt_if_a <- get_tptfv(data_if_a)
+mean_tpt_if_e <- get_tptfv(data_if_e)
+mean_tpt_if_n <- get_tptfv(data_if_n)
+
+#Sekunden
+mean_tptr_if_mo <- get_tptfvr(data_if_mo)
+mean_tptr_if_a <- get_tptfvr(data_if_a)
+mean_tptr_if_e <- get_tptfvr(data_if_e)
+mean_tptr_if_n <- get_tptfvr(data_if_n)
 
